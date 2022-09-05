a720303b3c6716c93a9eedbd36edefa5f57acbe4
MariaBurmeister <mariaburmeister@gmail.com>
2022-09-05 17:07:50 +0200

    chore(InputButton): assure inputId matches label

--- src/components/design-system/InputButton.tsx
+++ src/components/design-system/InputButton.tsx
@@ -1,10 +1,10 @@
 import { FunctionComponent, InputHTMLAttributes, Key } from "react";
-
+import { v4 as uuidv4 } from 'uuid';
 interface InputButtonProps extends InputHTMLAttributes<HTMLInputElement> {
   key?: Key;
 }
 export const InputButton: FunctionComponent<InputButtonProps> = ({type, children, id, checked, key, ...rest}) => {
-    const inputId = id;
+    const inputId = id || uuidv4();
     return( 
         <div className="input-as-button">
           <input 
