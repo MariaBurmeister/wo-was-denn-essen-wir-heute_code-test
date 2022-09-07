d193e75e98d1354df729a5fa2d1723153aea1541
MariaBurmeister <mariaburmeister@gmail.com>
2022-09-07 10:15:36 +0200

    chore(InputButton): restore acessibility labels

--- src/components/design-system/InputButton.tsx
+++ src/components/design-system/InputButton.tsx
@@ -1,19 +1,18 @@
 import { FunctionComponent, InputHTMLAttributes, Key } from "react";
-import { v4 as uuidv4 } from 'uuid';
 interface InputButtonProps extends InputHTMLAttributes<HTMLInputElement> {
   key?: Key;
 }
-export const InputButton: FunctionComponent<InputButtonProps> = ({type, children, id, checked, key, ...rest}) => {
-    const inputId = id || uuidv4();
+export const InputButton: FunctionComponent<InputButtonProps> = ({type, children, checked, onChange, ...rest}) => {
+    
     return( 
-        <div className="input-as-button">
+          <label  className={`input-as-button ${checked ? 'label_checked' : ''}`} >
+            {children}
           <input 
           {...rest}
-          id={inputId}
           type={type}
           checked={checked}
+          onChange={onChange}
          />
-          <label htmlFor={inputId} className={checked ? 'label_checked' : ''} >{children}</label>
-        </div>
+          </label>
     );
-    }
+}
