603f0153c06f54dfac9fcd20a19635e28c0201f3
MariaBurmeister <mariaburmeister@gmail.com>
2022-10-10 17:04:27 +0200

    chore(ErrorState): import from index

--- src/components/design-system/ErrorState.tsx
+++ src/components/design-system/ErrorState.tsx
@@ -1,4 +1,4 @@
 import { FunctionComponent, ReactNode } from "react";
-import { Illustration } from ".";
+import { Illustration } from "./Illustration";
 
 export const ErrorState: FunctionComponent<{message: string | ReactNode}> = ({message}) => <Illustration img="error" subtitle={message}/>;
