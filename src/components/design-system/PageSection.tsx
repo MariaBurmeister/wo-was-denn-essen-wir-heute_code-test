adc441a8a06ba1f5f72341d191e2649d85e0f7dc
MariaBurmeister <mariaburmeister@gmail.com>
2022-10-10 16:55:19 +0200

    chore(PageSection): identify sections by id

--- src/components/design-system/PageSection.tsx
+++ src/components/design-system/PageSection.tsx
@@ -1,14 +1,12 @@
-import { FunctionComponent, ReactNode } from "react";
+import { FunctionComponent, ReactNode} from "react";
 import './PageSection.scss';
 
 type HorizontalAlign = 'center' | 'left' | 'right';
 
 export const PageSection: FunctionComponent<{ title: string; undertitle?: string | ReactNode; headerActions?: ReactNode; children:ReactNode; xAlign?: HorizontalAlign}> = ({ title, undertitle, headerActions, children, xAlign = 'left' }) => {
 
-
-
     return (
-      <section className={`page_section${xAlign ? ' ' + xAlign : ''}`}>
+      <section id={title.toLowerCase()} className={`page_section${xAlign ? ' ' + xAlign : ''}`}>
         <header className={`page_section-header${xAlign ? ' ' + xAlign : ''}`}>
           <div className={`page_section-header-titles${xAlign ? ' ' + xAlign : ''}`}>
           <h2>{title}</h2>
