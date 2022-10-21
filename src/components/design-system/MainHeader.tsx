e98a44815a07dae4885d74c0322461f7fb8ec49b
MariaBurmeister <mariaburmeister@gmail.com>
2022-10-21 13:23:24 +0200

    feat(accessibility): implement accessible skip navigation link

--- src/components/design-system/MainHeader.tsx
+++ src/components/design-system/MainHeader.tsx
@@ -1,15 +1,15 @@
 import { Logo } from "./Logo";
 import { Navbar } from "./Navbar";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import './MainHeader.scss';
+import { SkipNavLink } from "../accesssibility";
 
 export const MainHeader = () => {
-
   return (
     <header className="main-header" aria-label='main-header'>
-        <Link aria-label="skip-navigation-link" className="sr-only" to="#main">Skip To Main Content</Link>
-        <Link aria-label='home-page' to="/"><Logo /></Link>
+        <SkipNavLink/>
+        <Link aria-hidden aria-label='home-page' to="/"><Logo /></Link>
         <Navbar navName="primary-navigation" >
             <Navbar.Item label='Home' to='/home'/>
             <Navbar.Item label='Dashboard' to='/dashboard'/>
