59332d76d08d6c5e72ea30e71bc7cd0e9a26d2da
MariaBurmeister <mariaburmeister@gmail.com>
2022-10-25 16:32:33 +0200

    chore(Title): extract undertitle to Title component

--- src/components/design-system/Title.tsx
+++ src/components/design-system/Title.tsx
@@ -3,8 +3,42 @@ import './Title.scss';
 
 export type TitleTag = 'h1'| 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
+type HorizontalAlign = 'center' | 'left' | 'right';
+
 type SectionId = string;
 type TitleLink = SectionId | 'self' ;
+
+interface TitleProps { 
+  title: string; 
+  variant: TitleTag; 
+  titleLinkTo?: TitleLink; 
+  undertitle?: string | ReactNode; 
+  horizontalAlign?: HorizontalAlign;
+}
+
+export const Title: FunctionComponent<TitleProps> = ({ title, variant, titleLinkTo, undertitle, horizontalAlign = 'left' }) => {
+  const linkToSelf = titleLinkTo === 'self';
+  const hash = linkToSelf ? title.toLowerCase().replace(/ /g, '-') : titleLinkTo;
+
+  return (
+    <>
+      {hash ? 
+
+      <div className={`title${horizontalAlign ? ' ' + horizontalAlign : ''}`}>
+        <a className={`self-link${horizontalAlign ? ' ' + horizontalAlign : ''}`} aria-label={`Link to current section`} href={`#${hash}`}>
+          {TitleMap[variant]({children:title, id:linkToSelf ? hash : undefined})} <span className={`hash ${variant}`} aria-hidden>#</span>
+        </a> 
+        {undertitle && <aside>{undertitle}</aside>}
+      </div> :
+      <div className={`title${horizontalAlign ? ' ' + horizontalAlign : ''}`}>
+        {TitleMap[variant]({children:title})}
+        <>{undertitle && <aside>{undertitle}</aside>}</>
+      </div>
+      }
+    </>
+  );
+};
+
 
 const TitleMap: Record<TitleTag, FunctionComponent<{children: ReactNode, id?: string}>> = { 
   h1: ({children, id}) => <h1 id={id}>{children}</h1>,
@@ -13,20 +47,4 @@ const TitleMap: Record<TitleTag, FunctionComponent<{children: ReactNode, id?: st
   h4: ({children, id}) => <h4 id={id}>{children}</h4>,
   h5: ({children, id}) => <h5 id={id}>{children}</h5>,
   h6: ({children, id}) => <h6 id={id}>{children}</h6>,
-};
-
-
-export const Title: FunctionComponent<{ title: string; variant: TitleTag; titleLinkTo?: TitleLink; }> = ({ title, variant, titleLinkTo }) => {
-  const linkToSelf = titleLinkTo === 'self';
-  const hash = linkToSelf ? title.toLowerCase().replace(/ /g, '-') : titleLinkTo;
-
-  return (
-    <>
-      {hash ? 
-        <a className='self-link' aria-label={`Link to current section`} href={`#${hash}`}>
-        {TitleMap[variant]({children:title, id:linkToSelf ? hash : undefined})} <span className={`hash ${variant}`} aria-hidden>#</span>
-        </a> : 
-        TitleMap[variant]({children:title})}
-    </>
-  );
 };
